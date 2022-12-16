e50371579e7b89cb5cd7f410b8c6c024780dc9da
amc131 <76466256+amc131@users.noreply.github.com>
2022-12-16 14:45:36 -0800

    Complete basic grid functionality

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
+/* eslint-disable no-plusplus */
 const gridContainer = document.querySelector('.grid-container');
+const gridNumber = 16;
+
+function createGrid(num) {
+  gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+
+  const numSquared = num * num;
+  for (let i = 0; i < numSquared; i++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.addEventListener('mouseover', () => {
+      grid.style.backgroundColor = 'black';
+    });
+    gridContainer.appendChild(grid);
+  }
+}
+createGrid(gridNumber);
